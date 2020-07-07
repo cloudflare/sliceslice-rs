f1f6b006475af7351d119d8c0bfbf355f7c3ed4f
Zak Cutner <me@zakcutner.uk>
2020-07-07 17:57:53 +0100

    Resolve various Clippy warnings

--- src/memcmp.rs
+++ src/memcmp.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 #[inline(always)]
 pub(crate) unsafe fn memcmp0(_: &[u8], _: &[u8]) -> bool {
     true
@@ -10,95 +12,86 @@ pub(crate) unsafe fn memcmp1(left: &[u8], right: &[u8]) -> bool {
 
 #[inline(always)]
 pub(crate) unsafe fn memcmp2(left: &[u8], right: &[u8]) -> bool {
-    let left = std::mem::transmute::<*const u8, *const u16>(left.as_ptr());
-    let right = std::mem::transmute::<*const u8, *const u16>(right.as_ptr());
+    let left = left.as_ptr().cast::<u16>();
+    let right = right.as_ptr().cast::<u16>();
     *left == *right
 }
 
-#[allow(dead_code)]
 #[inline(always)]
 pub(crate) unsafe fn memcmp3(left: &[u8], right: &[u8]) -> bool {
-    let left = std::mem::transmute::<*const u8, *const u32>(left.as_ptr());
-    let right = std::mem::transmute::<*const u8, *const u32>(right.as_ptr());
+    let left = left.as_ptr().cast::<u32>();
+    let right = right.as_ptr().cast::<u32>();
     (*left & 0x00ffffff) == (*right & 0x00ffffff)
 }
 
 #[inline(always)]
 pub(crate) unsafe fn memcmp4(left: &[u8], right: &[u8]) -> bool {
-    let left = std::mem::transmute::<*const u8, *const u32>(left.as_ptr());
-    let right = std::mem::transmute::<*const u8, *const u32>(right.as_ptr());
+    let left = left.as_ptr().cast::<u32>();
+    let right = right.as_ptr().cast::<u32>();
     *left == *right
 }
 
 #[inline(always)]
 pub(crate) unsafe fn memcmp5(left: &[u8], right: &[u8]) -> bool {
-    let left = std::mem::transmute::<*const u8, *const u64>(left.as_ptr());
-    let right = std::mem::transmute::<*const u8, *const u64>(right.as_ptr());
-    ((*left ^ *right) & 0x000000ffffffffff_u64) == 0
+    let left = left.as_ptr().cast::<u64>();
+    let right = right.as_ptr().cast::<u64>();
+    (*left ^ *right).trailing_zeros() >= 40
 }
 
 #[inline(always)]
 pub(crate) unsafe fn memcmp6(left: &[u8], right: &[u8]) -> bool {
-    let left = std::mem::transmute::<*const u8, *const u64>(left.as_ptr());
-    let right = std::mem::transmute::<*const u8, *const u64>(right.as_ptr());
-    ((*left ^ *right) & 0x0000ffffffffffff_u64) == 0
+    let left = left.as_ptr().cast::<u64>();
+    let right = right.as_ptr().cast::<u64>();
+    (*left ^ *right).trailing_zeros() >= 48
 }
 
 #[allow(dead_code)]
 #[inline(always)]
 pub(crate) unsafe fn memcmp7(left: &[u8], right: &[u8]) -> bool {
-    let left = std::mem::transmute::<*const u8, *const u64>(left.as_ptr());
-    let right = std::mem::transmute::<*const u8, *const u64>(right.as_ptr());
-    ((*left ^ *right) & 0x00ffffffffffffff_u64) == 0
+    let left = left.as_ptr().cast::<u64>();
+    let right = right.as_ptr().cast::<u64>();
+    (*left ^ *right).trailing_zeros() >= 56
 }
 
 #[inline(always)]
 pub(crate) unsafe fn memcmp8(left: &[u8], right: &[u8]) -> bool {
-    let left = std::mem::transmute::<*const u8, *const u64>(left.as_ptr());
-    let right = std::mem::transmute::<*const u8, *const u64>(right.as_ptr());
+    let left = left.as_ptr().cast::<u64>();
+    let right = right.as_ptr().cast::<u64>();
     *left == *right
 }
 
 #[inline(always)]
 pub(crate) unsafe fn memcmp9(left: &[u8], right: &[u8]) -> bool {
-    let left_ptr = left.as_ptr();
-    let right_ptr = right.as_ptr();
-    let left = std::mem::transmute::<*const u8, *const u64>(left_ptr);
-    let right = std::mem::transmute::<*const u8, *const u64>(right_ptr);
-    *left == *right && *left_ptr.add(8) == *right_ptr.add(8)
+    let left_first = left.as_ptr().cast::<u64>();
+    let right_first = right.as_ptr().cast::<u64>();
+    *left_first == *right_first && *left.as_ptr().add(8) == *right.as_ptr().add(8)
 }
 
 #[inline(always)]
 pub(crate) unsafe fn memcmp10(left: &[u8], right: &[u8]) -> bool {
-    let left_ptr = left.as_ptr();
-    let right_ptr = right.as_ptr();
-    let left = std::mem::transmute::<*const u8, *const u64>(left_ptr);
-    let right = std::mem::transmute::<*const u8, *const u64>(right_ptr);
-    let left2 = std::mem::transmute::<*const u8, *const u16>(left_ptr.add(8));
-    let right2 = std::mem::transmute::<*const u8, *const u16>(right_ptr.add(8));
-    *left == *right && *left2 == *right2
+    let left_first = left.as_ptr().cast::<u64>();
+    let right_first = right.as_ptr().cast::<u64>();
+    let left_second = left.as_ptr().add(8).cast::<u16>();
+    let right_second = right.as_ptr().add(8).cast::<u16>();
+    *left_first == *right_first && *left_second == *right_second
 }
 
 #[inline(always)]
 pub(crate) unsafe fn memcmp11(left: &[u8], right: &[u8]) -> bool {
-    let left_ptr = left.as_ptr();
-    let right_ptr = right.as_ptr();
-    let left = std::mem::transmute::<*const u8, *const u64>(left_ptr);
-    let right = std::mem::transmute::<*const u8, *const u64>(right_ptr);
-    let left2 = std::mem::transmute::<*const u8, *const u32>(left_ptr.add(8));
-    let right2 = std::mem::transmute::<*const u8, *const u32>(right_ptr.add(8));
-    *left == *right && (*left2 & 0x00ffffff) == (*right2 & 0x00ffffff)
+    let left_first = left.as_ptr().cast::<u64>();
+    let right_first = right.as_ptr().cast::<u64>();
+    let left_second = left.as_ptr().add(8).cast::<u32>();
+    let right_second = right.as_ptr().add(8).cast::<u32>();
+    *left_first == *right_first && (*left_second & 0x00ffffff) == (*right_second & 0x00ffffff)
 }
 
 #[inline(always)]
 pub(crate) unsafe fn memcmp12(left: &[u8], right: &[u8]) -> bool {
-    let left_ptr = left.as_ptr();
-    let right_ptr = right.as_ptr();
-    let left = std::mem::transmute::<*const u8, *const u64>(left_ptr);
-    let right = std::mem::transmute::<*const u8, *const u64>(right_ptr);
-    let left2 = std::mem::transmute::<*const u8, *const u32>(left_ptr.add(8));
-    let right2 = std::mem::transmute::<*const u8, *const u32>(right_ptr.add(8));
-    *left == *right && *left2 == *right2
+    let left_first = left.as_ptr().cast::<u64>();
+    let right_first = right.as_ptr().cast::<u64>();
+    let left_second = left.as_ptr().add(8).cast::<u32>();
+    let right_second = right.as_ptr().add(8).cast::<u32>();
+    *left_first == *right_first && *left_second == *right_second
 }
 
 #[inline(always)]
