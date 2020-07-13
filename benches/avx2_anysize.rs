07537aadd2d98d1f1204ae8d0709efa5b3ac3cdf
Zak Cutner <me@zakcutner.uk>
2020-07-13 11:17:13 +0100

    Update benchmarks to use different needle and haystack sizes

--- benches/avx2_anysize.rs
+++ benches/avx2_anysize.rs
@@ -1,276 +1,118 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use memmem::{Searcher, TwoWaySearcher};
-use std::fs::File;
-use std::io::{self, BufRead, Read};
+use std::str;
 use strstr::avx2::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut f = File::open("./data/i386.txt").unwrap();
-    let mut buffer = Vec::new();
-    f.read_to_end(&mut buffer).unwrap();
-    let content = String::from_utf8_lossy(&buffer);
+    let haystack = include_bytes!("../data/haystack");
+    let needle = include_bytes!("../data/needle");
 
-    let file = File::open("./data/words").unwrap();
-    let mut words: Vec<String> = io::BufReader::new(file)
-        .lines()
-        .filter_map(|line| line.ok())
-        .filter(|word| word.len() > 1)
-        .collect();
-    // Sort all words by length then lexicographically
-    words.sort_by(|a, b| {
-        if a.len() != b.len() {
-            a.len().partial_cmp(&b.len()).unwrap()
-        } else {
-            a.partial_cmp(b).unwrap()
+    let sizes = [1, 5, 10, 20, 50, 100, 1000];
+
+    for (i, &size) in sizes.iter().enumerate() {
+        let mut group = c.benchmark_group(format!("{}-byte needle", size));
+        let needle = &needle[..size];
+
+        for &size in &sizes[i..] {
+            let parameter = &format!("{}-byte haystack", size);
+            let haystack = &haystack[..size];
+
+            group.bench_with_input(
+                BenchmarkId::new("String::find", parameter),
+                &size,
+                |b, _| {
+                    let haystack = str::from_utf8(haystack).unwrap();
+                    let needle = str::from_utf8(needle).unwrap();
+                    b.iter(|| haystack.find(needle));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("TwoWaySearcher::search_in", parameter),
+                &size,
+                |b, _| {
+                    let searcher = TwoWaySearcher::new(needle);
+                    b.iter(|| black_box(searcher.search_in(haystack)));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("twoway::find_bytes", parameter),
+                &size,
+                |b, _| {
+                    b.iter(|| black_box(twoway::find_bytes(haystack, needle)));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("strstr_avx2_original", parameter),
+                &size,
+                |b, _| {
+                    b.iter(|| black_box(unsafe { strstr_avx2_original(haystack, needle) }));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("strstr_avx2_rust_simple", parameter),
+                &size,
+                |b, _| {
+                    b.iter(|| black_box(unsafe { strstr_avx2_rust_simple(haystack, needle) }));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("strstr_avx2_rust_simple_2", parameter),
+                &size,
+                |b, _| {
+                    b.iter(|| black_box(unsafe { strstr_avx2_rust_simple_2(haystack, needle) }));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("strstr_avx2_rust_fast", parameter),
+                &size,
+                |b, _| {
+                    b.iter(|| black_box(unsafe { strstr_avx2_rust_fast(haystack, needle) }));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("strstr_avx2_rust_fast_2", parameter),
+                &size,
+                |b, _| {
+                    b.iter(|| black_box(strstr_avx2_rust_fast_2(haystack, needle)));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("strstr_avx2_rust_aligned", parameter),
+                &size,
+                |b, _| {
+                    b.iter(|| black_box(unsafe { strstr_avx2_rust_aligned(haystack, needle) }));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("StrStrAVX2Searcher::search_in", parameter),
+                &size,
+                |b, _| {
+                    let searcher = StrStrAVX2Searcher::new(needle);
+                    b.iter(|| black_box(searcher.search_in(haystack)));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("DynamicAvx2Searcher::search_in", parameter),
+                &size,
+                |b, _| {
+                    let searcher = DynamicAvx2Searcher::new(needle.to_owned().into_boxed_slice());
+                    b.iter(|| black_box(searcher.search_in(haystack)));
+                },
+            );
         }
-    });
-    let words = words;
 
-    let twoway_words: Vec<TwoWaySearcher<'_>> = words
-        .iter()
-        .map(|word| TwoWaySearcher::new(word.as_bytes()))
-        .collect();
-
-    let avx2_words: Vec<StrStrAVX2Searcher> = words
-        .iter()
-        .map(|word| StrStrAVX2Searcher::new(word.as_bytes()))
-        .collect();
-
-    let dynamic_avx2_searchers: Vec<DynamicAvx2Searcher> = words
-        .iter()
-        .map(|word| DynamicAvx2Searcher::new(word.clone().into_boxed_str().into()))
-        .collect();
-
-    // Benchmarks against long haystacks
-
-    c.bench_function("String::find with long haystack", |b| {
-        b.iter(|| {
-            for word in &words {
-                content.find(word);
-            }
-        })
-    });
-
-    c.bench_function(
-        "memmem::TwoWaySearcher::search_in with long haystack",
-        |b| {
-            b.iter(|| {
-                for twoway_word in &twoway_words {
-                    twoway_word.search_in(content.as_bytes());
-                }
-            })
-        },
-    );
-
-    c.bench_function("twoway::find_bytes with long haystack", |b| {
-        b.iter(|| {
-            for word in &words {
-                twoway::find_bytes(content.as_bytes(), word.as_bytes());
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_original with long haystack", |b| {
-        b.iter(|| {
-            for word in &words {
-                unsafe {
-                    strstr_avx2_original(content.as_bytes(), word.as_bytes());
-                }
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_rust_simple with long haystack", |b| {
-        b.iter(|| {
-            for word in &words {
-                unsafe {
-                    strstr_avx2_rust_simple(content.as_bytes(), word.as_bytes());
-                }
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_rust_simple_2 with long haystack", |b| {
-        b.iter(|| {
-            for word in &words {
-                unsafe {
-                    strstr_avx2_rust_simple_2(content.as_bytes(), word.as_bytes());
-                }
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_rust_fast with long haystack", |b| {
-        b.iter(|| {
-            for word in &words {
-                unsafe {
-                    strstr_avx2_rust_fast(content.as_bytes(), word.as_bytes());
-                }
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_rust_fast_2 with long haystack", |b| {
-        b.iter(|| {
-            for word in &words {
-                strstr_avx2_rust_fast_2(content.as_bytes(), word.as_bytes());
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_rust_aligned with long haystack", |b| {
-        b.iter(|| {
-            for word in &words {
-                unsafe {
-                    strstr_avx2_rust_aligned(content.as_bytes(), word.as_bytes());
-                }
-            }
-        })
-    });
-
-    c.bench_function("StrStrAVX2Searcher::search_in with long haystack", |b| {
-        b.iter(|| {
-            for avx2_word in &avx2_words {
-                avx2_word.search_in(content.as_bytes());
-            }
-        })
-    });
-
-    c.bench_function("DynamicAvx2Searcher::search_in with long haystack", |b| {
-        b.iter(|| {
-            for searcher in &dynamic_avx2_searchers {
-                searcher.search_in(content.as_bytes());
-            }
-        })
-    });
-
-    // Benchmarks against short haystacks
-    //
-    // Since words are ordered by length, pick a word as needle
-    // and use bigger words as haystacks.
-
-    c.bench_function("String::find with short haystack", |b| {
-        b.iter(|| {
-            for (i, word) in words.iter().enumerate() {
-                for content in &words[(i + 1)..] {
-                    content.find(word);
-                }
-            }
-        })
-    });
-
-    c.bench_function(
-        "memmem::TwoWaySearcher::search_in with short haystack",
-        |b| {
-            b.iter(|| {
-                for (i, word) in twoway_words.iter().enumerate() {
-                    for content in &words[(i + 1)..] {
-                        word.search_in(content.as_bytes());
-                    }
-                }
-            })
-        },
-    );
-
-    c.bench_function("twoway::find_bytes with short haystack", |b| {
-        b.iter(|| {
-            for (i, word) in words.iter().enumerate() {
-                for content in &words[(i + 1)..] {
-                    twoway::find_bytes(content.as_bytes(), word.as_bytes());
-                }
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_original with short haystack", |b| {
-        b.iter(|| {
-            for (i, word) in words.iter().enumerate() {
-                for content in &words[(i + 1)..] {
-                    unsafe {
-                        strstr_avx2_original(content.as_bytes(), word.as_bytes());
-                    }
-                }
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_rust_simple with short haystack", |b| {
-        b.iter(|| {
-            for (i, word) in words.iter().enumerate() {
-                for content in &words[(i + 1)..] {
-                    unsafe {
-                        strstr_avx2_rust_simple(content.as_bytes(), word.as_bytes());
-                    }
-                }
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_rust_simple_2 with short haystack", |b| {
-        b.iter(|| {
-            for (i, word) in words.iter().enumerate() {
-                for content in &words[(i + 1)..] {
-                    unsafe {
-                        strstr_avx2_rust_simple_2(content.as_bytes(), word.as_bytes());
-                    }
-                }
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_rust_fast with short haystack", |b| {
-        b.iter(|| {
-            for (i, word) in words.iter().enumerate() {
-                for content in &words[(i + 1)..] {
-                    unsafe {
-                        strstr_avx2_rust_fast(content.as_bytes(), word.as_bytes());
-                    }
-                }
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_rust_fast_2 with short haystack", |b| {
-        b.iter(|| {
-            for (i, word) in words.iter().enumerate() {
-                for content in &words[(i + 1)..] {
-                    strstr_avx2_rust_fast_2(content.as_bytes(), word.as_bytes());
-                }
-            }
-        })
-    });
-
-    c.bench_function("strstr_avx2_rust_aligned with short haystack", |b| {
-        b.iter(|| {
-            for (i, word) in words.iter().enumerate() {
-                for content in &words[(i + 1)..] {
-                    unsafe {
-                        strstr_avx2_rust_aligned(content.as_bytes(), word.as_bytes());
-                    }
-                }
-            }
-        })
-    });
-
-    c.bench_function("StrStrAVX2Searcher::search_in with short haystack", |b| {
-        b.iter(|| {
-            for (i, word) in avx2_words.iter().enumerate() {
-                for content in &words[(i + 1)..] {
-                    word.search_in(content.as_bytes());
-                }
-            }
-        })
-    });
-
-    c.bench_function("DynamicAvx2Searcher::search_in with short haystack", |b| {
-        b.iter(|| {
-            for (i, searcher) in dynamic_avx2_searchers.iter().enumerate() {
-                for content in &words[(i + 1)..] {
-                    searcher.search_in(content.as_bytes());
-                }
-            }
-        })
-    });
+        group.finish();
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
