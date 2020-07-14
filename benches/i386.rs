7aa92c6a6c3c2e8352cc803a0e75ba08f320a93b
Zak Cutner <me@zakcutner.uk>
2020-07-14 16:20:29 +0100

    Extend tests and benchmarks completeness

---
+++ benches/i386.rs
@@ -0,0 +1,291 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use memmem::{Searcher, TwoWaySearcher};
+use std::{
+    fs::{self, File},
+    io::{BufRead, BufReader},
+};
+use strstr::avx2::*;
+
+fn search_short_haystack(c: &mut Criterion) {
+    let mut needles = BufReader::new(File::open("data/words.txt").unwrap())
+        .lines()
+        .map(Result::unwrap)
+        .filter(|needle| needle.len() > 1)
+        .collect::<Vec<_>>();
+    needles.sort_unstable_by_key(|needle| needle.len());
+    let needles = needles.iter().map(String::as_str).collect::<Vec<_>>();
+
+    let mut group = c.benchmark_group("short_haystack");
+
+    group.bench_function("String::find", |b| {
+        b.iter(|| {
+            for (i, needle) in needles.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(haystack.find(needle));
+                }
+            }
+        });
+    });
+
+    group.bench_function("TwoWaySearcher::search_in", |b| {
+        let searchers = needles
+            .iter()
+            .map(|needle| TwoWaySearcher::new(needle.as_bytes()))
+            .collect::<Vec<_>>();
+
+        b.iter(|| {
+            for (i, searcher) in searchers.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(searcher.search_in(haystack.as_bytes()));
+                }
+            }
+        });
+    });
+
+    group.bench_function("twoway::find_bytes", |b| {
+        b.iter(|| {
+            for (i, needle) in needles.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(twoway::find_bytes(haystack.as_bytes(), needle.as_bytes()));
+                }
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_original", |b| {
+        b.iter(|| {
+            for (i, needle) in needles.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(unsafe {
+                        strstr_avx2_original(haystack.as_bytes(), needle.as_bytes())
+                    });
+                }
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_rust_simple", |b| {
+        b.iter(|| {
+            for (i, needle) in needles.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(unsafe {
+                        strstr_avx2_rust_simple(haystack.as_bytes(), needle.as_bytes())
+                    });
+                }
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_rust_simple_2", |b| {
+        b.iter(|| {
+            for (i, needle) in needles.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(unsafe {
+                        strstr_avx2_rust_simple_2(haystack.as_bytes(), needle.as_bytes())
+                    });
+                }
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_rust_fast", |b| {
+        b.iter(|| {
+            for (i, needle) in needles.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(unsafe {
+                        strstr_avx2_rust_fast(haystack.as_bytes(), needle.as_bytes())
+                    });
+                }
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_rust_fast_2", |b| {
+        b.iter(|| {
+            for (i, needle) in needles.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(strstr_avx2_rust_fast_2(
+                        haystack.as_bytes(),
+                        needle.as_bytes(),
+                    ));
+                }
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_rust_aligned", |b| {
+        b.iter(|| {
+            for (i, needle) in needles.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(unsafe {
+                        strstr_avx2_rust_aligned(haystack.as_bytes(), needle.as_bytes())
+                    });
+                }
+            }
+        });
+    });
+
+    group.bench_function("StrStrAVX2Searcher::search_in", |b| {
+        let searchers = needles
+            .iter()
+            .map(|&needle| StrStrAVX2Searcher::new(needle.as_bytes()))
+            .collect::<Vec<_>>();
+
+        b.iter(|| {
+            for (i, searcher) in searchers.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(searcher.search_in(haystack.as_bytes()));
+                }
+            }
+        });
+    });
+
+    group.bench_function("DynamicAvx2Searcher::search_in", |b| {
+        let searchers = needles
+            .iter()
+            .map(|&needle| {
+                DynamicAvx2Searcher::new(needle.as_bytes().to_owned().into_boxed_slice())
+            })
+            .collect::<Vec<_>>();
+
+        b.iter(|| {
+            for (i, searcher) in searchers.iter().enumerate() {
+                for haystack in &needles[i..] {
+                    black_box(searcher.search_in(haystack.as_bytes()));
+                }
+            }
+        });
+    });
+
+    group.finish();
+}
+
+fn search_long_haystack(c: &mut Criterion) {
+    let haystack = fs::read("data/i386.txt").unwrap();
+    let haystack = String::from_utf8_lossy(&haystack);
+
+    let needles = BufReader::new(File::open("data/words.txt").unwrap())
+        .lines()
+        .map(Result::unwrap)
+        .filter(|needle| needle.len() > 1)
+        .collect::<Vec<_>>();
+
+    let mut group = c.benchmark_group("long_haystack");
+
+    group.bench_function("String::find", |b| {
+        b.iter(|| {
+            for needle in &needles {
+                black_box(haystack.find(needle));
+            }
+        });
+    });
+
+    group.bench_function("TwoWaySearcher::search_in", |b| {
+        let searchers = needles
+            .iter()
+            .map(|needle| TwoWaySearcher::new(needle.as_bytes()))
+            .collect::<Vec<_>>();
+
+        b.iter(|| {
+            for searcher in &searchers {
+                black_box(searcher.search_in(haystack.as_bytes()));
+            }
+        });
+    });
+
+    group.bench_function("twoway::find_bytes", |b| {
+        b.iter(|| {
+            for needle in &needles {
+                black_box(twoway::find_bytes(haystack.as_bytes(), needle.as_bytes()));
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_original", |b| {
+        b.iter(|| {
+            for needle in &needles {
+                black_box(unsafe { strstr_avx2_original(haystack.as_bytes(), needle.as_bytes()) });
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_rust_simple", |b| {
+        b.iter(|| {
+            for needle in &needles {
+                black_box(unsafe {
+                    strstr_avx2_rust_simple(haystack.as_bytes(), needle.as_bytes())
+                });
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_rust_simple_2", |b| {
+        b.iter(|| {
+            for needle in &needles {
+                black_box(unsafe {
+                    strstr_avx2_rust_simple_2(haystack.as_bytes(), needle.as_bytes())
+                });
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_rust_fast", |b| {
+        b.iter(|| {
+            for needle in &needles {
+                black_box(unsafe { strstr_avx2_rust_fast(haystack.as_bytes(), needle.as_bytes()) });
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_rust_fast_2", |b| {
+        b.iter(|| {
+            for needle in &needles {
+                black_box(strstr_avx2_rust_fast_2(
+                    haystack.as_bytes(),
+                    needle.as_bytes(),
+                ));
+            }
+        });
+    });
+
+    group.bench_function("strstr_avx2_rust_aligned", |b| {
+        b.iter(|| {
+            for needle in &needles {
+                black_box(unsafe {
+                    strstr_avx2_rust_aligned(haystack.as_bytes(), needle.as_bytes())
+                });
+            }
+        });
+    });
+
+    group.bench_function("StrStrAVX2Searcher::search_in", |b| {
+        let searchers = needles
+            .iter()
+            .map(|needle| StrStrAVX2Searcher::new(needle.as_bytes()))
+            .collect::<Vec<_>>();
+
+        b.iter(|| {
+            for searcher in &searchers {
+                black_box(searcher.search_in(haystack.as_bytes()));
+            }
+        });
+    });
+
+    group.bench_function("DynamicAvx2Searcher::search_in", |b| {
+        let searchers = needles
+            .iter()
+            .map(|needle| DynamicAvx2Searcher::new(needle.as_bytes().to_owned().into_boxed_slice()))
+            .collect::<Vec<_>>();
+
+        b.iter(|| {
+            for searcher in &searchers {
+                black_box(searcher.search_in(haystack.as_bytes()));
+            }
+        });
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, search_short_haystack, search_long_haystack);
+criterion_main!(benches);
---
+++ tests/i386.rs
@@ -0,0 +1,73 @@
+use memmem::{Searcher, TwoWaySearcher};
+use std::{
+    fs::{self, File},
+    io::{BufRead, BufReader},
+};
+use strstr::avx2::*;
+
+fn search(haystack: &str, needle: &str) {
+    let result = haystack.find(&needle).is_some();
+
+    let haystack = haystack.as_bytes();
+    let needle = needle.as_bytes();
+
+    let searcher = TwoWaySearcher::new(needle);
+    assert_eq!(searcher.search_in(haystack).is_some(), result);
+
+    assert_eq!(twoway::find_bytes(haystack, needle).is_some(), result);
+
+    assert_eq!(unsafe { strstr_avx2_original(haystack, needle) }, result);
+
+    assert_eq!(unsafe { strstr_avx2_rust_simple(haystack, needle) }, result);
+
+    assert_eq!(
+        unsafe { strstr_avx2_rust_simple_2(haystack, needle) },
+        result
+    );
+
+    assert_eq!(unsafe { strstr_avx2_rust_fast(haystack, needle) }, result);
+
+    assert_eq!(strstr_avx2_rust_fast_2(haystack, needle), result);
+
+    assert_eq!(
+        unsafe { strstr_avx2_rust_aligned(haystack, needle) },
+        result
+    );
+
+    let searcher = StrStrAVX2Searcher::new(needle);
+    assert_eq!(searcher.search_in(haystack), result);
+
+    let searcher = DynamicAvx2Searcher::new(needle.to_owned().into_boxed_slice());
+    assert_eq!(searcher.search_in(haystack), result);
+}
+
+#[test]
+fn search_short_haystack() {
+    let mut needles = BufReader::new(File::open("data/words.txt").unwrap())
+        .lines()
+        .map(Result::unwrap)
+        .filter(|needle| needle.len() > 1)
+        .collect::<Vec<_>>();
+    needles.sort_unstable_by_key(|needle| needle.len());
+
+    for (i, needle) in needles.iter().enumerate() {
+        for haystack in &needles[i..] {
+            search(haystack, needle);
+        }
+    }
+}
+
+#[test]
+fn search_long_haystack() {
+    let haystack = fs::read("data/i386.txt").unwrap();
+    let haystack = String::from_utf8_lossy(&haystack);
+
+    let needles = BufReader::new(File::open("data/words.txt").unwrap())
+        .lines()
+        .map(Result::unwrap)
+        .filter(|needle| needle.len() > 1);
+
+    for needle in needles {
+        search(&haystack, &needle);
+    }
+}
